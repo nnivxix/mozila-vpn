367b59221866851dac6875beb8d6ea7a2cb142ad
nnivxix <adhe.hanasa@gmail.com>
2021-02-08 11:46:22 +0700

    add carousel animation on header

--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@ import "./style.scss";
 // import { Tooltip, Toast, Popover } from 'bootstrap';
 import "bootstrap";
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  //convert to Array ref https://www.gavsblog.com/blog/htmlcollection-foreach-loop-convert-object-to-array-javascript
+  let slides = Array.from(document.getElementsByClassName("slides")) ;
+  slides.forEach( slide => {
+  slide.style.display = "none"; 
+ // console.log(slide);
+  })
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }    
+
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
