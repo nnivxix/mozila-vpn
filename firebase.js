283bbf99930d968699a2beacb7479a4a74965614
nnivxix <adhe.hanasa@gmail.com>
2021-02-09 14:24:40 +0700

    revisi for responsive

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBulW6mtqEyBRPxj4m7PCewWPX-tNskNpM",
+  authDomain: "mozilla-vpn.firebaseapp.com",
+  projectId: "mozilla-vpn",
+  storageBucket: "mozilla-vpn.appspot.com",
+  messagingSenderId: "77346973584",
+  appId: "1:77346973584:web:a1e438b62e7aaec5a57515",
+  measurementId: "G-P55WLF34SG"
+};
